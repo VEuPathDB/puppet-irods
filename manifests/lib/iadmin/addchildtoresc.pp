92cdfe3f234bf77bd4f32869f2e5a42118503b3c
Mark Heiges <mheiges@uga.edu>
2016-06-16 11:06:14 -0400

    add logging to iadmin commands, primarily for debugging ordering

--- manifests/lib/iadmin/addchildtoresc.pp
+++ manifests/lib/iadmin/addchildtoresc.pp
@@ -4,10 +4,12 @@ define irods::lib::iadmin::addchildtoresc (
   $chld = undef,
 ) {
 
+  $command = "iadmin addchildtoresc ${resc} ${chld}"
+
   exec { $name:
     path        => '/usr/bin',
     environment => ["HOME=/root"],
-    command     => "iadmin addchildtoresc ${resc} ${chld}",
+    command     => "$command; echo $command >> /var/lib/irods/.puppet_iadmin.log",
     unless      => "iadmin lr ${chld} |grep -q 'resc_parent: ${resc}'"
   }
 
--- manifests/lib/iadmin/mkresc.pp
+++ manifests/lib/iadmin/mkresc.pp
@@ -6,11 +6,12 @@ define irods::lib::iadmin::mkresc (
   $ctxs = undef,
 ) {
 
-notify{ "iadmin mkresc ${resc} type ${path} ${ctxs}" :}
+  $command = "iadmin mkresc ${resc} ${type} ${path} ${ctxs}"
+
   exec { $name:
     path    => '/usr/bin',
     environment => ["HOME=/root"],
-    command => "iadmin mkresc ${resc} ${type} ${path} ${ctxs}",
+    command => "$command; echo $command >> /var/lib/irods/.puppet_iadmin.log",
     onlyif  => "iadmin lr ${resc} |grep -q 'No rows found'"
  }
 
